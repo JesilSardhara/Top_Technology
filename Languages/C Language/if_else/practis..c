a268aab206051418634ebdc6d6a627b8c95782c1
JesilSardhara <jecsardhara@gmail.com>
2026-07-19 17:34:25 +0530

    Top_Technology

---
+++ Languages/C Language/if_else/practis..c
@@ -0,0 +1,144 @@
+#include<stdio.h>
+
+int main(){
+	
+	int a,b,c,year,mark,age,divi;
+	
+	printf("enter a number 1:");
+	scanf("%d",&a);
+	printf("enter a number 2:");
+	scanf("%d",&b);
+	printf("enter a number 3:");
+	scanf("%d",&c);
+	
+//	printf("enter a number 3:");
+//	scanf("%d",&);
+	
+	
+//	printf("enter a year:");
+//	scanf("%d",&year);
+
+//	printf("enter a age:");
+//	scanf("%d",&age);
+//	
+//	printf("enter a test marks:");
+//	scanf("%d",&mark);	
+	
+//	if(a % 2 == 0){
+//		printf("even number");
+//	}
+//	else{
+//		printf("odd number");
+//	}
+	
+//	if(a >0 ){
+//		printf("positive number");
+//	}
+//	else if(a == 0){
+//		printf("reenter a value");
+//	}
+//	else{
+//		printf("negetive number");
+//	}
+	
+//	if(a>b){
+//		printf("a is bit:%d",a);
+//	}
+//	else if(a == b){
+//		printf("same number");
+//	}
+//	else{
+//		printf("b is big:%d",b);
+//	}
+
+//	if(a >=18){
+//		printf("eligibal");
+//	}
+//	else{
+//		printf("not eligibal");
+//	}
+
+//	if(a >=35){
+//		printf("pass");
+//	}
+//	else{
+//		printf("fail");
+//	}
+
+//	if((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)){
+//		printf("This is leap year");
+//	}
+//	else{
+//		printf("this is not leap year");
+//	}
+	
+//	if(a>100){
+//		printf("this is big number:%d",a);
+//	}
+//	else{
+//		printf("rthis number is small:%d",a);
+//	}
+	
+//	if(mark >=90 && mark <=100){
+//		printf("A+ Grade :%d",mark);
+//	}
+//	else if(mark >= 80 && mark <= 89 ){
+//		printf("A Grade :%d",mark);
+//	}
+//	else if(mark >= 70 && mark <= 79){
+//		printf("b Grade :%d",mark);
+//	}
+//	else if(mark >= 60 && mark <= 69 ){
+//		printf("c Grade :%d",mark);
+//	}
+//	else if(mark >= 35 && mark <=59){
+//		printf("pass :%d",mark);
+//	}
+//	else{
+//		printf("fail");
+//	}
+	
+//	if(age >=18){
+//		if(mark >= 13 && mark <=20){
+//			printf("License Approved");
+//		}
+//		else{
+//			printf("pls enter valid marks:%d",mark);
+//		}
+//	}
+//	else{
+//		printf("not eligibal");
+//	}
+	
+//	if(a > b && a > c){
+//		printf("a is big:%d",a);
+//	}
+//	else if(a<b && b>c){
+//		printf("b is big:%d",b);
+//	}
+//	else{
+//		printf("c is big:%d",c);
+//	}
+
+//	if (a + b > c && a + c > b && b + c > a){
+//		printf("This is made a tringle");
+//	}
+//	else{
+//		printf("not made a tringle");
+//	}
+	
+//	if (a >= 35 && b >= 35 && c >= 35){
+//		mark = a+b+c;
+//		divi = mark/3;
+//		printf("you pass\n your persantag:%d",divi);
+//	}
+//	else{
+//		printf("you Fail");
+//	}
+		
+	
+	
+	return 0;
+}
+
+
