48ea10196bd16fc7cf2cf2082b1ea28d567fa608
JesilSardhara <jecsardhara@gmail.com>
2026-07-21 22:55:56 +0530

    Top_Technology

---
+++ Languages/C Language/faunction/task/factorial.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+
+int total(int n){
+	int i,sum = 0;
+	for(i=1;i<=n;i++){
+		sum = sum + i;
+	}
+	return sum;	
+}
+
+void main()
+{
+	int num;
+	printf("Enter your num : ");
+	scanf("%d",&num);
+	
+	printf("Num of value : %d",num);
+	printf("\nTotal Sum : %d",total(num));
+	
+	getch();
+}
