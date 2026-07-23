1cb6313c47e2238e1176548665d9b97a60fc2e5a
JesilSardhara <jecsardhara@gmail.com>
2026-07-23 17:15:43 +0530

    Top_Technology

--- Languages/C Language/array/practis.c
+++ Languages/C Language/array/practis.c
@@ -2,18 +2,33 @@
 
 void main(){
 
-	int i,a[4] ;
+	int i,a[4],temp,j ;
 	
 	for(i=0;i<=4;i++){
 		printf("enter a value :");
 		scanf("%d",&a[i]);
 	}
 	
-//	for(i=0;i<=4;i++){
-//		printf("a[%d] : %d\ns",i,a[i]);
+	for(i=0;i<=4;i++){
+		printf("a[%d] : %d\ns",i,a[i]);
+	}
+//	for(i=4;i>=0;i--){
+//		printf("a[%d]:%d\n",i,a[i]);
 //	}
-	for(i=4;i>=0;i--){
-		printf("a[%d]:%d\n",i,a[i]);
+	
+	for(i=0;i<=4;i++){
+		for(j=0;j<=4;j++){
+			if(a[i] > a[j]){
+				temp =a[i];
+				a[i] = a[j];
+				a[j] = temp;
+			}
+		}
+	}
+	printf("\n\n");
+	
+	for(i=0;i<=4;i++){
+		printf("a[%d] : %d\ns",i,a[i]);
 	}
 	
 	getch();
