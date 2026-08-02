4e4481996b5b21718507d7bde994cdb1e5f48479
JesilSardhara <jecsardhara@gmail.com>
2026-08-02 11:08:22 +0530

    Top_Technology

---
+++ Assignments/Assignment_2/session 2/hallow.c
@@ -0,0 +1,7 @@
+int main()
+{
+    printf("Hello, Instagram World!");
+
+    return 0;	
+}
+
---
+++ Assignments/Assignment_2/session 2/hello.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("Hello, Instagram World!");
+
+    return 0;
+}
