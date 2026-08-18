f7b99ba85268e067e97e537e98e1e70a98aa79c4
JesilSardhara <jecsardhara@gmail.com>
2026-08-18 12:30:11 +0530

    Top_Technology

---
+++ Assignments/Assignment_4_sql/session_1/3_question.sql
@@ -0,0 +1,9 @@
+use instaclone;
+
+INSERT INTO Users (user_id, username, email, followers_count)
+VALUES
+(1, 'jesil_dev', 'jesil@gmail.com', 1200),
+(2, 'tech_with_raj', 'raj@gmail.com', 3500),
+(3, 'insta_girl_23', 'girl23@gmail.com', 5800);
+
+select * from users;
