d4acbb0ae56c71b7b23e22552da421e4ae13c4da
JesilSardhara <jecsardhara@gmail.com>
2026-08-19 13:59:08 +0530

    Top_Technology

---
+++ Assignments/Assignment_4_sql/session_2/3_question.sql
@@ -0,0 +1,3 @@
+use instaclone;
+
+DROP TABLE restaurants;
---
+++ Assignments/Assignment_4_sql/session_2/4_question.sql
@@ -0,0 +1,10 @@
+use instaclone;
+
+-- CREATE TABLE playlists (
+--     playlist_id INT PRIMARY KEY AUTO_INCREMENT,
+--     title VARCHAR(50),
+--     created_by VARCHAR(50),
+--     created_at DATE
+-- );
+
+select * from playlists;
