b88e774fce947de3955ac41d7c18879c689462c3
JesilSardhara <jecsardhara@gmail.com>
2026-09-20 10:45:42 +0530

    Top_Technology

---
+++ Languages/JavaScript/bom.js
@@ -0,0 +1,5 @@
+// window.alert("sometext");
+// window.confirm("are you soure")
+// window.prompt("get input")
+// window.open("https://www.google.com");
+window.print("this is my priint")
---
+++ Languages/JavaScript/dom.js
@@ -0,0 +1,22 @@
+
+// dom : Document Object model
+
+// 1) console log : 
+    // console.log("Hello Javascript");
+     // console.log("Hello react")
+     // console.log(10+20)
+    // console.log("mul :",50*20)
+
+// 2) document .write()
+    // document.write("Hello javascript")
+    // document.write("<br>Hello React")
+
+// 3) getelemetbyId  :  innerText : only text allow
+
+    console.log(document.getElementById('hero'))
+    document.getElementById('hero').innerText = "hello js"
+
+// 4) : innerHTML : text and tag both allow"
+
+    console.log(document.getElementById('name'))
+    document.getElementById('name').innerHTML =" <b> My name is Html in js</b>"
