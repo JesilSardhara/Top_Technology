56bbdd6f3d978d02e9cb7c0cd3b06c89837760ac
JesilSardhara <jecsardhara@gmail.com>
2026-09-24 14:00:18 +0530

    Top_Technology

---
+++ Languages/JavaScript/task/script.js
@@ -0,0 +1,91 @@
+let display = document.getElementById("display");
+
+// 0 to 9
+function number(num) {
+
+    display.value = display.value + num;
+
+}
+
+// +
+function add() {
+
+    display.value = display.value + "+";
+
+}
+
+// -
+function sub() {
+
+    display.value = display.value + "-";
+
+}
+
+// *
+function multiply() {
+
+    display.value = display.value + "*";
+
+}
+
+// /
+function divide() {
+
+    display.value = display.value + "/";
+
+}
+
+// C
+function clearDisplay() {
+
+    display.value = "";
+
+}
+
+// =
+function calculate() {
+
+    let value = display.value;
+
+    if (value.includes("+")) {
+
+        let numbers = value.split("+");
+
+        let num1 = parseInt(numbers[0]);
+        let num2 = parseInt(numbers[1]);
+
+        display.value = num1 + num2;
+
+    }
+    else if (value.includes("-")) {
+
+        let numbers = value.split("-");
+
+        let num1 = parseInt(numbers[0]);
+        let num2 = parseInt(numbers[1]);
+
+        display.value = num1 - num2;
+
+    }
+    else if (value.includes("*")) {
+
+        let numbers = value.split("*");
+
+        let num1 = parseInt(numbers[0]);
+        let num2 = parseInt(numbers[1]);
+
+        display.value = num1 * num2;
+
+    }
+    else if (value.includes("/")) {
+
+        let numbers = value.split("/");
+
+        let num1 = parseInt(numbers[0]);
+        let num2 = parseInt(numbers[1]);
+
+        display.value = num1 / num2;
+
+    }
+
+}
